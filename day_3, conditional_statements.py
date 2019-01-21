05061b8164ad53e703befcb53fc3ca9161df9d76
Wambui <cmcayo4@gmail.com>
2019-01-21 16:12:44 +0300

    Day 3, Conditional Statements

---
+++ day_3, conditional_statements.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    N = int(input())
+    if N % 2 == 1:
+        print ('Weird')
+    elif N < 5:
+        print('Not Weird')
+    elif N <= 20:
+        print('Weird')
+    else:
+        print('Not Weird')
