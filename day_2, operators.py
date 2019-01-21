a669b2854e4b88e7d7e9c1704b1799587270898e
Wambui <cmcayo4@gmail.com>
2019-01-21 15:38:51 +0300

    Day 2, Operators

---
+++ day_2, operators.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    if __name__ == '__main__':
+        meal_cost = float(input())
+
+        tip_percent = int(input())
+
+        tax_percent = int(input())
+
+        tip = meal_cost * (tip_percent / 100)
+
+        tax = meal_cost * (tax_percent / 100)
+
+        total_cost = int(round(meal_cost + tip + tax))
+
+        print(total_cost)
+
+    solve(meal_cost, tip_percent, tax_percent)
